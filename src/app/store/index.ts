2c941ebc256e4383704237ef392bbf13968d2340
moroz-art-dev <moroz.art.dev@gmail.com>
2024-05-24 18:52:05 +0300

    redux hooks

---
+++ src/app/store/index.ts
@@ -0,0 +1,6 @@
+import rootReducer from './rootReducer';
+import store, {RootState, AppDispatch} from './store';
+
+export {rootReducer};
+export default store;
+export type {RootState, AppDispatch};
